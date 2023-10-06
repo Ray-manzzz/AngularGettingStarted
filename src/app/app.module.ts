1d01acb5ead5726ac329c1edcbf2411b22e18622
Raymond Forshew <raymond.forshew@logicsoftware.co.uk>
2023-10-06 09:39:39 +0100

    Separation of responsibilities using modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { FormsModule } from '@angular/forms';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductsDetailComponent } from './products/products-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductsDetailGuard } from './products/products-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductsDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([ //Angular Routing related work, order matters
-      { path: 'products', component: ProductListComponent },
-      { 
-        path: 'products/:id', 
-        canActivate: [ProductsDetailGuard],
-        component: ProductsDetailComponent 
-      },
+      
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-  ])
+  ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
